63d2b31f1ca1ef19242d88ec3fb7a258a86854ae
Samuel <diogosamuelfernandes@outlook.com>
2020-04-13 16:15:23 +0100

    Initialize Terminal

---
+++ src/main/java/Application.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Application {
+    public static void main(String[] args) throws IOException {
+        Game mainGame = new Game();
+    }
+}
+
+
---
+++ src/main/java/Game.java
@@ -0,0 +1,25 @@
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Game {
+    private Screen screen;
+
+    public Game() {
+        try {
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            screen = new TerminalScreen(terminal);
+
+            screen.setCursorPosition(null);   // we don't need a cursor
+            screen.startScreen();             // screens must be started
+            screen.doResizeIfNecessary();     // resize screen if necessary
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
