b8b3ef2f93157472424e2103b2fc81d8c76bf84c
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2020-04-27 18:29:47 +0100

    Hero model cant change hero position

--- src/main/java/Model/Elements/Hero.java
+++ src/main/java/Model/Elements/Hero.java
@@ -6,20 +6,4 @@ public class Hero extends ElementModel {
     public Hero(Position position) {
         super("H", "#000000", position);
     }
-
-    public Position moveUp() {
-        return new Position(super.getPosition().getX(), super.getPosition().getY() - 1);
-    }
-
-    public Position moveDown() {
-        return new Position(super.getPosition().getX(), super.getPosition().getY() + 1);
-    }
-
-    public Position moveRight() {
-        return new Position(super.getPosition().getX() + 1, super.getPosition().getY());
-    }
-
-    public Position moveLeft() {
-        return new Position(super.getPosition().getX() - 1, super.getPosition().getY());
-    }
 }
