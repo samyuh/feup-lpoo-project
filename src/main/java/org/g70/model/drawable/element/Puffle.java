0e2bbb3befc804354f63c000ac8a32d3acb70d1e
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2020-05-22 15:55:14 +0100

    We're dumb

--- src/main/java/org/g70/model/drawable/element/Puffle.java
+++ src/main/java/org/g70/model/drawable/element/Puffle.java
@@ -1,11 +1,11 @@
 package org.g70.model.drawable.element;
 
-import org.g70.controller.level.interact.InteractPuffle;
+import org.g70.controller.level.interact.InteractStop;
 import org.g70.model.Position;
 
 public class Puffle extends ElementModel {
     public Puffle(Position position) {
         super("\u25CF", "#ff0422", position);
-        setInteraction(new InteractPuffle(this));
+        setInteraction(new InteractStop(this));
     }
 }
