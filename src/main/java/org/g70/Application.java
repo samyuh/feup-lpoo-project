093ccd40e3b704b0296c7425e99ea175dc28cde9
Diogo Samuel Fernandes <diogosamuelfernandes@outlook.com>
2020-05-27 18:05:25 +0100

    More bug fixes on tests

--- src/main/java/org/g70/Application.java
+++ src/main/java/org/g70/Application.java
@@ -3,11 +3,12 @@ package org.g70;
 import org.g70.controller.MainController;
 import org.g70.view.ScreenView;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class Application {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException, FontFormatException {
         ScreenView gui = new ScreenView(30, 20);
 
         MainController controller = new MainController(gui);
